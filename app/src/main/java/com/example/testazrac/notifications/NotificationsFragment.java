33cf5f55ce9df132c6cb020543a2a723ace8cb59
DSGog <dmitriy.amsec@gmail.com>
2024-08-29 09:06:05 +0400

    Подписи иконок в меню навигации

--- app/src/main/java/com/example/testazrac/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/testazrac/notifications/NotificationsFragment.java
@@ -13,29 +13,29 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.testazrac.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
-
-    private FragmentNotificationsBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        textView.setText(""); // Очистите текст при создании фрагмента
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (binding != null) {
-            binding.textNotifications.setText(""); // Очистите текст при уничтожении представления
-        }
-        binding = null;
-    }
+//
+//    private FragmentNotificationsBinding binding;
+//
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//        NotificationsViewModel notificationsViewModel =
+//                new ViewModelProvider(this).get(NotificationsViewModel.class);
+//
+//        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.textNotifications;
+//        textView.setText(""); // Очистите текст при создании фрагмента
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
+//    }
+//
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        if (binding != null) {
+//            binding.textNotifications.setText(""); // Очистите текст при уничтожении представления
+//        }
+//        binding = null;
+//    }
 }
--- app/src/main/java/com/example/testazrac/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/testazrac/notifications/NotificationsViewModel.java
@@ -5,19 +5,19 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class NotificationsViewModel extends ViewModel {
-
-    private final MutableLiveData<String> mText;
-
-    public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue(""); // Изначально пустой текст
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
-
-    public void resetText() {
-        mText.setValue(""); // Метод для сброса текста
-    }
+//
+//    private final MutableLiveData<String> mText;
+//
+//    public NotificationsViewModel() {
+//        mText = new MutableLiveData<>();
+//        mText.setValue(""); // Изначально пустой текст
+//    }
+//
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
+//
+//    public void resetText() {
+//        mText.setValue(""); // Метод для сброса текста
+//    }
 }
